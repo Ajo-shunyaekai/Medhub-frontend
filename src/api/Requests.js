e65796b21a3b67b2e05f21bea60b098bc6accddf
Ajo-shunyaekai <Ajo@shunyaekai.tech>
2024-12-30 14:33:51 +0530

    update: api request

--- src/api/Requests.js
+++ src/api/Requests.js
@@ -1,45 +1,20 @@
 import axios from 'axios';
-import { user_type } from '../constants';
 axios.defaults.baseURL                       = process.env.REACT_APP_API_URL;
 axios.defaults.headers.post['Content-Type']  = 'application/json';
 axios.defaults.headers.post['authorization'] = process.env.REACT_APP_Authorization;
 // axios.defaults.withCredentials               = true
-
+ 
 export const postRequest = async (URL, requestData, callback) => {
     try {
         const response  = await axios.post(URL, requestData);
         // return response.data;
         return callback(response.data);
-
+ 
     } catch (err) {
         return callback({code : 500, message : 'Connection faild, please start node server'});
     }
 }
-
-export const getRequest = async (URL, requestData, callback) => {
-    try {
-        const response = await axios.get(URL, {
-            params: requestData, // Attach any query params if needed
-            headers: {
-                Authorization: process.env.REACT_APP_Authorization || '',
-                'Content-Type': 'application/json',
-                access_token: sessionStorage.getItem('token') , // Include tokens if required
-            },
-            withCredentials: true, // Ensure credentials are sent
-        });
-
-        console.log('GET Request URL:', URL);
-        console.log('Response:', response.data);
-
-        return callback(response.data);
-
-    } catch (err) {
-        console.error('Error in GET Request:', err);
-        return callback({ code: 500, message: 'Connection failed, please start node server' });
-    }
-};
-
-
+ 
 export const postRequestWithFile = async (URL, requestData, callback) => {
     try {
         const response = await axios({
@@ -47,23 +22,24 @@ export const postRequestWithFile = async (URL, requestData, callback) => {
             url     : URL,
             data    : requestData,
             headers : {
-                "access_token" : sessionStorage.getItem('token') ,
-                "buyer_id"     : sessionStorage.getItem('buyer_id') ,
-                "supplier_id"  : sessionStorage.getItem("supplier_id"),
-                "admin_id"     : sessionStorage.getItem("admin_id"),
+                "access_token" : sessionStorage.getItem('token') || localStorage.getItem('token') || undefined,
+                "buyer_id"     :  sessionStorage.getItem('buyer_id') || localStorage.getItem('buyer_id') || undefined,
+                "supplier_id"  :  sessionStorage.getItem('supplier_id') || localStorage.getItem('supplier_id') || undefined,
+                "admin_id"  :  sessionStorage.getItem('admin_id') || localStorage.getItem('admin_id') || undefined,
                 "Content-Type" : "multipart/form-data",                
-                "user_type"    : user_type,
+                "user_type" : (sessionStorage.getItem('buyer_id') || localStorage.getItem('buyer_id')) ? "Buyer" : 
+                (sessionStorage.getItem('supplier_id') || localStorage.getItem('supplier_id')) ? "Supplier" : (sessionStorage.getItem('admin_id') || localStorage.getItem('admin_id')) ? "Admin" : (sessionStorage.getItem('seller_id') || localStorage.getItem('seller_id')) ? "Seller" : undefined,
             }
         });
         // return response.data;
         return callback(response.data);
-
+ 
     } catch (err) {
         return callback({code : 500, message : 'Connection faild, please start node server'});
         // throw err;
     }
 }
-
+ 
 export const postRequestWithToken = async (URL, requestData, callback) => {
     try {
         const response = await axios({
@@ -72,27 +48,28 @@ export const postRequestWithToken = async (URL, requestData, callback) => {
             data    : requestData,
             // withCredentials : true,
             headers : {
-                "access_token" : sessionStorage.getItem('token') ,
-                "buyer_id"     : sessionStorage.getItem('buyer_id') ,
-                "supplier_id"  : sessionStorage.getItem("supplier_id"),
-                "admin_id"     : sessionStorage.getItem("admin_id"),
+                "access_token" : sessionStorage.getItem('token') || localStorage.getItem('token') || undefined,
+                "buyer_id"     :  sessionStorage.getItem('buyer_id') || localStorage.getItem('buyer_id') || undefined,
+                "supplier_id"  :  sessionStorage.getItem('supplier_id') || localStorage.getItem('supplier_id') || undefined,
+                "admin_id"  :  sessionStorage.getItem('admin_id') || localStorage.getItem('admin_id') || undefined,
                 "Content-Type" : "application/json",
-                "user_type"    : user_type,
+                "user_type" : (sessionStorage.getItem('buyer_id') || localStorage.getItem('buyer_id')) ? "Buyer" : 
+                (sessionStorage.getItem('supplier_id') || localStorage.getItem('supplier_id')) ? "Supplier" : (sessionStorage.getItem('admin_id') || localStorage.getItem('admin_id')) ? "Admin" : (sessionStorage.getItem('seller_id') || localStorage.getItem('seller_id')) ? "Seller" : undefined,
             } 
         });
         
         if(response.status == 401){ 
             sessionStorage.clear();
-
+ 
         } else {  // if(response.status == 200)
             return callback(response.data);
-
+ 
         } 
     } catch (err) {
         return callback({code : 500, message : 'Connection failed, please start node server '});
     }
 }
-
+ 
 export const postRequestWithTokenAndFile = async (URL, requestData, callback) => {
     try {
         const response = await axios({
@@ -100,22 +77,23 @@ export const postRequestWithTokenAndFile = async (URL, requestData, callback) =>
             url     : URL,
             data    : requestData,
             headers : {
-                "access_token" : sessionStorage.getItem('token') ,
-                "buyer_id"     : sessionStorage.getItem('buyer_id') ,
-                "supplier_id"  : sessionStorage.getItem("supplier_id"),
-                "admin_id"     : sessionStorage.getItem("admin_id"),
+                "access_token" : sessionStorage.getItem('token') || localStorage.getItem('token') || undefined,
+                "buyer_id"     :  sessionStorage.getItem('buyer_id') || localStorage.getItem('buyer_id') || undefined,
+                "supplier_id"  :  sessionStorage.getItem('supplier_id') || localStorage.getItem('supplier_id') || undefined,
+                "admin_id"  :  sessionStorage.getItem('admin_id') || localStorage.getItem('admin_id') || undefined,
                 "Content-Type" : "multipart/form-data",                
-                "user_type"    : user_type,
+                "user_type" : (sessionStorage.getItem('buyer_id') || localStorage.getItem('buyer_id')) ? "Buyer" : 
+                (sessionStorage.getItem('supplier_id') || localStorage.getItem('supplier_id')) ? "Supplier" : (sessionStorage.getItem('admin_id') || localStorage.getItem('admin_id')) ? "Admin" : (sessionStorage.getItem('seller_id') || localStorage.getItem('seller_id')) ? "Seller" : undefined,
             }
         });
         return callback(response.data);
-
+ 
     } catch (err) {
         return callback({code : 500, message : 'Connection faild, please start node server '});
-
+ 
     }
 }
-
+ 
 export const checkAuth = async () => {
 }
-
+ 
