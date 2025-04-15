bc03c2ad9af6c3f332d443785075fa11d87164af
rishabhshunyaekai <rishabhkumar@shunyaekai.tech>
2025-04-15 17:42:43 +0530

    optimize code

--- src/LogisticsPanel/LogisticsRoutes/Router.js
+++ src/LogisticsPanel/LogisticsRoutes/Router.js
@@ -2,109 +2,88 @@ import React, { Suspense, lazy } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Loader from "../components/SharedComponents/Loader/Loader";
 
-const AddVehicle = lazy(() =>
-  import("../components/Vehicle/NewAddVehicle/NewAddVehicle")
-);
-const ActiveOrder = lazy(() =>
-  import("../components/Orders/ActiveOrders/ActiveOrder")
-);
-const CompleteOrder = lazy(() =>
-  import("../components/Orders/CompletedOrders/CompleteOrder")
-);
-const Dashboard = lazy(() => import("../components/Dashboard/NewDashboard"));
-const Inventory = lazy(() =>
-  import("../components/Inventory/NewInventoryList/NewInventoryList")
-);
-const LogisticsLayout = lazy(() =>
-  import("../components/SharedComponents/LogisticsLayout")
-);
-const Login = lazy(() => import("../components/Login/Login"));
-const LogisticsDetails = lazy(() =>
-  import("../components/Orders/NewOrderDetails/NewOrderDetails")
-);
-const Orders = lazy(() => import("../components/Orders/NewOrder"));
-const PickupOrders = lazy(() =>
-  import("../components/PickupOrders/NewPickupOrder/NewPickupOrder")
-);
-const PickupDetails = lazy(() =>
-  import(
-    "../components/PickupOrders/NewPickupOrderDetails/NewPickupOrderDetails"
-  )
-);
-const PendingOrder = lazy(() =>
-  import("../components/Orders/PendingOrders/PendingOrders")
-);
-const Profile = lazy(() =>
-  import("../components/SharedComponents/Profile/Profile")
-);
-const Shipment = lazy(() => import("../components/Shipment/NewShipment"));
-const Tracking = lazy(() =>
-  import("../components/Tracking/NewTrackingForm/NewTrackingForm")
-);
-const VehicleList = lazy(() =>
-  import("../components/Vehicle/NewVehicleList/NewVehicleList")
-);
+const AddVehicle        = lazy(() => import("../components/Vehicle/NewAddVehicle/NewAddVehicle"));
+const Dashboard         = lazy(() => import("../components/Dashboard/NewDashboard"));
+const Error             = lazy(() => import("../components/SharedComponents/Error/Error"));
+const Inventory         = lazy(() => import("../components/Inventory/NewInventoryList/InventoryForm"));
+const LogisticsLayout   = lazy(() => import("../components/SharedComponents/LogisticsLayout"));
+const Login             = lazy(() => import("../components/Login/Login"));
+const LogisticsDetails  = lazy(() => import("../components/Orders/NewOrderDetails/NewOrderDetails"));
+const Orders            = lazy(() => import("../components/Orders/NewOrder"));
+const PickupOrders      = lazy(() => import("../components/PickupOrders/NewPickupOrder/NewPickupOrder"));
+const PickupDetails     = lazy(() => import("../components/PickupOrders/NewPickupOrderDetails/NewPickupOrderDetails"));
+const Profile           = lazy(() => import("../components/SharedComponents/Profile/Profile"));
+const Shipment          = lazy(() => import("../components/Shipment/NewShipment"));
+const Tracking          = lazy(() => import("../components/Tracking/NewTrackingForm/NewTrackingForm"));
+const VehicleList       = lazy(() => import("../components/Vehicle/NewVehicleList/NewVehicleList"));
+// const ActiveOrder       = lazy(() => import("../components/Orders/ActiveOrders/ActiveOrder"));
+// const CompleteOrder     = lazy(() => import("../components/Orders/CompletedOrders/CompleteOrder"));
+// const PendingOrder      = lazy(() => import("../components/Orders/PendingOrders/PendingOrders"));
 
 const router = createBrowserRouter([
   {
-    path: "/logistics/login",
-    element: <Login />,
+    path    : "/logistics/login",
+    element : <Login />,
   },
   {
-    path: "/logistics",
+    path     : "/logistics",
     // element  : <Layout />,
-    element: <LogisticsLayout />,
-    children: [
+    element  : <LogisticsLayout />,
+    children : [
       {
-        index: true,
-        element: <Dashboard />,
+        index   : true,
+        element: <Dashboard />
       },
       {
-        path: "order",
-        element: <Orders />,
-        children: [
-          { path: "active", element: <ActiveOrder /> },
-          { path: "completed", element: <CompleteOrder /> },
-          { path: "pending", element: <PendingOrder /> },
-        ],
+        path     : "order",
+        element  : <Orders />,
+        // children : [
+        //   { path : "active",     element: <ActiveOrder /> },
+        //   { path : "completed",  element: <CompleteOrder /> },
+        //   { path : "pending",    element: <PendingOrder /> },
+        // ],
       },
       {
-        path: "logistics-details/:requestId",
-        element: <LogisticsDetails />,
+        path    : "logistics-details/:requestId",
+        element : <LogisticsDetails />,
       },
       {
-        path: "pickup-order",
-        element: <PickupOrders />,
+        path    : "pickup-order",
+        element : <PickupOrders />,
       },
       {
-        path: "pickup-order-details/:requestId",
-        element: <PickupDetails />,
+        path    : "pickup-order-details/:requestId",
+        element : <PickupDetails />,
       },
       {
-        path: "inventory",
-        element: <Inventory />,
+        path    : "inventory",
+        element : <Inventory />,
       },
       {
-        path: "add-vehicle",
-        element: <AddVehicle />,
+        path    : "add-vehicle",
+        element : <AddVehicle />,
       },
       {
-        path: "vehicle-list",
-        element: <VehicleList />,
+        path    : "vehicle-list",
+        element : <VehicleList />,
       },
       {
-        path: "shipment",
-        element: <Shipment />,
+        path    : "shipment",
+        element : <Shipment />,
       },
       {
-        path: "tracking",
-        element: <Tracking />,
+        path    : "tracking",
+        element : <Tracking />,
       },
       {
-        path: "profile",
-        element: <Profile />,
+        path    : "profile",
+        element : <Profile />,
       },
     ],
+  },
+  {
+    path    : '*',
+    element : <Error />
   },
 ]);
 
