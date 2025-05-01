e78da4d5ddeacb541f17a290c756177b39d8fb69
rishabhshunyaekai <rishabhkumar@shunyaekai.tech>
2025-05-01 11:13:02 +0530

    Added Login module.css in buyer/supplier

--- src/Supplier/components/SharedComponents/Signup/SupplierLogin.js
+++ src/Supplier/components/SharedComponents/Signup/SupplierLogin.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import './login.css';
+// import './login.css';
+import styles from './Supplierlogin.module.css';
 import logo from '../../../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
 import { apiRequests } from '../../../../api/index';
@@ -169,17 +170,17 @@ const SupplierLogin = ({socket}) => {
       }
 
     return (
-        <div className='login-main-container'>
-            <div className='login-container-logo-section'>
+        <div className={styles.loginMainContainer}>
+            <div className={styles.loginContainerLogoSection}>
                 <img src={logo} alt="Logo" />
             </div>
-            <div className='login-container-form-section'>
-                <div className='login-container-form-section-heading'>Supplier Login</div>
-                <form className='login-main-form-section' onSubmit={handleSubmit}>
-                    <div className='login-form-main-div'>
-                        <label className='login-form-main-label'>Email ID</label>
+            <div className={styles.loginContainerFormSection}>
+                <div className={styles.loginContainerFormSectionHeading}>Supplier Login</div>
+                <form className={styles.loginMainFormSection} onSubmit={handleSubmit}>
+                    <div className={styles.loginFormMainDiv}>
+                        <label className={styles.loginFormMainLabel}>Email ID</label>
                         <input
-                            className='login-form-main-input'
+                            className={styles.loginFormMainInput}
                             autoComplete='off'
                             type='text'
                             name='email'
@@ -187,13 +188,13 @@ const SupplierLogin = ({socket}) => {
                             value={email}
                             onChange={handleEmailChange}
                         />
-                        {errors.email && <span className="login-errors">{errors.email}</span>}
+                        {errors.email && <span className={styles.loginErrors}>{errors.email}</span>}
                     </div>
-                    <div className='login-form-main-div'>
-                        <label className='login-form-main-label'>Password</label>
-                        <div className='login-form-input-eye-container'>
+                    <div className={styles.loginFormMainDiv}>
+                        <label className={styles.loginFormMainLabel}>Password</label>
+                        <div className={styles.loginFormInputEyeContainer}>
                             <input
-                                className='login-form-main-input'
+                                className={styles.loginFormMainInput}
                                 type={showPassword ? 'text' : 'password'}
                                 autoComplete='off'
                                 name='password'
@@ -202,39 +203,39 @@ const SupplierLogin = ({socket}) => {
                                 onChange={handlePasswordChange}
                             />
                             {showPassword ? (
-                                <VisibilityOffIcon className='login-form-input-eye-icons' onClick={togglePasswordVisibility} />
+                                <VisibilityOffIcon className={styles.loginFormInputEyeIcons} onClick={togglePasswordVisibility} />
                             ) : (
-                                <VisibilityIcon className='login-form-input-eye-icons' onClick={togglePasswordVisibility} />
+                                <VisibilityIcon className={styles.loginFormInputEyeIcons} onClick={togglePasswordVisibility} />
                             )}
                         </div>
-                        {errors.password && <span className="login-errors">{errors.password}</span>}
+                        {errors.password && <span className={styles.loginErrors}>{errors.password}</span>}
                     </div>
                     <Link to='/supplier/forgot-password'>
-                    <div className='login-form-main-div'>
-                        <span className='login-form-main-password'>Forgot Password?</span>
+                    <div className={styles.loginFormMainDiv}>
+                        <span className={styles.loginFormMainPassword}>Forgot Password?</span>
                     </div>
                     </Link>
-                    <div className='login-form-main-buttons'>
+                    <div className={styles.loginFormMainButtons}>
                         {/* <button type='button' className='login-form-main-cancel' onClick={handleCancel}>Cancel</button> */}
                         <button 
                         type='submit' 
-                        className='login-form-main-login'
+                        className={styles.loginFormMainLogin}
                         disabled={loading}
                         >
                             {/* Login */}
                             {loading ? (
-                                <div className='loading-spinner'></div> 
+                                <div className={styles.loadingSpinner}></div> 
                             ) : (
                                 'Login'
                             )}
                             </button>
                     </div>
                 </form>
-                <div className="header__center">OR</div>
-                <div className='login-form-main-signup'>
-                    <span className='login__signup__content'>Don't have an account?</span>
+                <div className={styles.headerCenter}>OR</div>
+                <div className={styles.loginFormMainSignup}>
+                    <span className={styles.loginSignupContent}>Don't have an account?</span>
                     <Link to='/supplier/sign-up'>
-                        <span className='login__signup__here'>&nbsp;Sign up here</span>
+                        <span className={styles.loginSignupHere}>&nbsp;Sign up here</span>
                     </Link>
                 </div>
             </div>
