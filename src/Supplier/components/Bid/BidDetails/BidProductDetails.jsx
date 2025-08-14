dde4b969d7ea11dac4b442065d80d6534704f339
Ravi-set <rk@shunyaekai.tech>
2025-08-14 12:02:46 +0530

    Add Product Name dropdown to Bid Product Details and update Bid History of supplier panel

--- src/Supplier/components/Bid/BidDetails/BidProductDetails.jsx
+++ src/Supplier/components/Bid/BidDetails/BidProductDetails.jsx
@@ -8,19 +8,25 @@ import {
   fetchBidById,
 } from "../../../../redux/reducers/bidSlice";
 import ProductList from "./ProductList";
+import { useNavigate } from "react-router-dom";
+import { fetchProductsList } from "../../../../redux/reducers/productSlice";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 import { useFormik } from "formik";
 import BidHistoryList from "./BidHistoryList";
+import Select from 'react-select'
+import { Tooltip } from "react-tooltip";
 
 const BidDetails = () => {
   const { id, itemId } = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const participantId = localStorage.getItem("_id");
   const [participatingDetails, setParticipatingeDetails] = useState({});
   const { bidDetails, loading } = useSelector(
     (state) => state?.bidReducer || {}
   );
+  const [productList, setProductList] = useState([]);
   const [itemDetails, setItemDetails] = useState({});
   const [fieldsArray, setFieldsArray] = useState([]);
   const [isEditing, setIsEditing] = useState(false); // Set initial value of isEditing to false
@@ -30,11 +36,21 @@ const BidDetails = () => {
   const [initialValues, setInitialValues] = useState({
     amount: "",
     timeLine: "",
-    tnc:""
+    tnc:"",
+    productName:"",
+    productId:""
   });
 
   //to handle cancel, manage seperate state with initial value
-  const [savedValue, setSavedValue] = useState({amount:"",timeLine:"",tnc:""});
+  const [savedValue, setSavedValue] = useState({amount:"",timeLine:"",tnc:"",productName:"",productId:""});
+
+  /* select option added */
+ /*  const option = [
+    { value: '2332', label: 'Paracetamol'},
+    { value: '2331', label: 'Algoliptin'},
+    { value: '2330', label: 'Crocin'},
+    { value: '2335', label: 'Crocin Tablet'},
+  ] */
   
   // Validation schema using Yup
   const validationSchema = Yup.object({
@@ -47,6 +63,8 @@ const BidDetails = () => {
       .typeError("Timeline must be a number")
       .positive("Timeline must be a positive")
       .integer("Timeline must be an integer"),
+    productName: Yup.string()
+      .required("Product Name is required.")
   });
 
   // Formik form initialization
@@ -54,16 +72,20 @@ const BidDetails = () => {
     initialValues: initialValues,
     validationSchema: validationSchema,
     onSubmit: (values) => {
+      
       try {
         const obj = {
           participantId: participantId,
           supplier_id: localStorage.getItem('supplier_id'),
           amount: Number(values.amount),
           tnc: values.tnc,
+          productName: values.productName,
+          productId: values.productId,
           timeLine: Number(values.timeLine),
           bidId: id,
           itemId: itemId,
         };
+        
         dispatch(updateBidProductDetails(obj)).then((response) => {
           if (response?.meta.requestStatus === "fulfilled") {
             // setLoading(false);
@@ -150,12 +172,16 @@ const BidDetails = () => {
         amount: participatingDetails?.amount,
         timeLine: participatingDetails?.timeLine,
         tnc: participatingDetails?.tnc,
+        productName: participatingDetails?.productName,
+        productId: participatingDetails?.productId
       });
 
       setSavedValue({
         amount: participatingDetails?.amount,
         timeLine: participatingDetails?.timeLine,
         tnc: participatingDetails?.tnc,
+        productName: participatingDetails?.productName,
+        productId: participatingDetails?.productId
       });
 
       setIsEditing(false); // If participatingDetails are found, editing is disabled
@@ -183,7 +209,7 @@ const BidDetails = () => {
       formik.setValues(savedValue);
     }
     else{
-      setSavedValue({amount:"",timeLine:"",tnc:""});
+      setSavedValue({amount:"",timeLine:"",tnc:"",productName:""});
       formik.setValues(savedValue);
     }
   }
@@ -193,6 +219,50 @@ const BidDetails = () => {
   );
 
   const isDisabled = !participatingDetails && allFieldsEmpty;
+
+  //api for fetching productId and name
+  
+  useEffect(() => {
+      const supplierIdSessionStorage = localStorage?.getItem("_id");
+      const supplierIdLocalStorage = localStorage?.getItem("_id");
+  
+      if (!supplierIdSessionStorage && !supplierIdLocalStorage) {
+        localStorage?.clear();
+        navigate("/buyer/login");
+        return;
+      }
+      const fetchData = async () => {
+        try {
+          const supplierId =
+            localStorage?.getItem("_id") ||
+            localStorage?.getItem("_id");
+          if (!supplierId) {
+            localStorage?.clear();
+            navigate("/supplier/login");
+            return;
+          }
+  
+          const fetchProducts = async () => {
+            // setLoading(true);
+            const response = await dispatch(
+              fetchProductsList({
+                url: `product/for-dd?supplier_id=${supplierId}&showDuplicate=false`,
+              })
+            );
+            if (response.meta.requestStatus === "fulfilled") {
+              setProductList(response?.payload?.products || []);
+              // setLoading(false);
+            }
+          };
+          fetchProducts();
+        } catch (error) {
+          console.error("Error fetching products:", error.message);
+        } finally {
+          // setLoading(false);
+        }
+      };
+      fetchData();
+    }, []);
 
   return (
     <div className={styles.container}>
@@ -312,61 +382,192 @@ const BidDetails = () => {
                       onSubmit={formik?.handleSubmit}
                     >
                       <div className={isEditing?styles.newFieldForm : styles.fieldForm}>
-                        <div className={isEditing?styles.fieldDiv:styles.newFieldDiv}>
+                        {/* select product name */}
+                        {
+                          isEditing && (
+                            <div className={isEditing?styles.fieldDiv:styles.newFieldDiv}>
                           <label className={styles.fieldFormLabel}>
-                            {getFieldLabel("Bid Price")}
+                            {getFieldLabel("Product Name")}
                             {isEditing && (
                               <span className={styles.labelStamp}>*</span>
                             )}
                           </label>
-                          <div className={isEditing?styles.newFieldFormDiv:styles.fieldFormDiv}>
-                            <input
-                              name="amount"
-                              type="numeric"
-                              placeholder="Enter Bid Price"
-                              value={isEditing?(formik?.values.amount||""):(`${formik?.values.amount} USD`) }
-                              onBlur={formik?.handleBlur}
-                              onChange={formik?.handleChange}
-                              disabled={!isEditing}
-                              className={isEditing ? styles.fieldFormInput: styles.fieldFormNonEditInput}
-                            />
-                            {formik?.errors.amount && formik?.touched.amount && (
+                          <Select
+                          options={productList || []}
+                          value={
+                            formik.values.productId
+                              ? { label: formik.values.productName, value: formik.values.productId }
+                              : null
+                          }
+                          onChange={(selectedOption) => {
+                            formik.setFieldValue("productName", selectedOption?.label || "");
+                            formik.setFieldValue("productId", selectedOption?.value || "");
+                          }}
+                          styles={{
+                            control: (base) => ({
+                              ...base,
+                              width: '100%',
+                              padding:'3.3px',
+                              outline: 'none',
+                              border: 'none',
+                              backgroundColor: '#ffffff !important',
+                              fontSize: '0.825rem',
+                              color: '#5e565f !important',
+                              borderRadius: '4px',
+                              cursor:"pointer !important",
+                              boxShadow:
+                                'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,' +
+                                'rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+                            }),
+                            placeholder: (base) => ({
+                              ...base,
+                              color: '#5e565f !important', 
+                              fontSize: '0.825rem',
+                              fontWeight: '400 !important'
+                            }),
+                            option: (base, state) => ({
+                              ...base,
+                              color:'#5e565f !important',
+                              fontSize: '0.825rem',
+                              padding: '10px',
+                            }),
+
+                          }}
+                          placeholder='select product name from column'
+                          />
+
+                          {formik?.errors.productName && formik?.touched.productName && (
                               <div className={styles.fieldError}>
-                                {formik?.errors.amount}
+                                {formik?.errors.productName}
                               </div>
-                            )}
-                          </div>
+                          )}
                         </div>
-                        <div className={isEditing?styles.fieldDiv:styles.newFieldDiv}>
-                          <label className={styles.fieldFormLabel}>
-                            {getFieldLabel("Timeline")}
-                            {isEditing && (
-                              <span className={styles.labelStamp}>*</span>
-                            )}
-                          </label>
-                          <div className={isEditing?styles.newFieldFormDiv:styles.fieldFormDiv}>
-                            <input
-                              name="timeLine"
-                              type="numeric"
-                              placeholder="Enter the expected delivery duration (in days)"
-                              value={isEditing?(formik?.values.timeLine || ""):(`${formik?.values?.timeLine} Days`)}
-                              onBlur={formik?.handleBlur}
-                              onChange={formik?.handleChange}
-                              disabled={!isEditing}
-                              className={isEditing ? styles.fieldFormInput: styles.fieldFormNonEditInput}
-                            />
-                            {formik?.errors.timeLine &&
-                              formik?.touched.timeLine && (
-                                <div className={styles.fieldError}>
-                                  {formik?.errors.timeLine}
+                          )
+                        }
+                        {/* Enter Bid Price */}
+                        {
+                          isEditing && (
+                            <div className={isEditing?styles.fieldDiv:styles.newFieldDiv}>
+                              <label className={styles.fieldFormLabel}>
+                                {getFieldLabel("Bid Price")}
+                                {isEditing && (
+                                  <span className={styles.labelStamp}>*</span>
+                                )}
+                              </label>
+                              <div className={isEditing?styles.newFieldFormDiv:styles.fieldFormDiv}>
+                                <input
+                                  name="amount"
+                                  type="numeric"
+                                  placeholder="Enter Bid Price"
+                                  value={isEditing?(formik?.values.amount||""):(`${formik?.values.amount} USD`) }
+                                  onBlur={formik?.handleBlur}
+                                  onChange={formik?.handleChange}
+                                  disabled={!isEditing}
+                                  className={isEditing ? styles.fieldFormInput: styles.fieldFormNonEditInput}
+                                />
+                                {formik?.errors.amount && formik?.touched.amount && (
+                                  <div className={styles.fieldError}>
+                                    {formik?.errors.amount}
+                                  </div>
+                                )}
+                              </div>
+                            </div>
+                          )
+                        }
+                        {/* Enter Timeline */}
+                        {
+                          isEditing && (
+                             <div className={isEditing?styles.fieldDiv:styles.newFieldDiv}>
+                                <label className={styles.fieldFormLabel}>
+                                  {getFieldLabel("Timeline")}
+                                  {isEditing && (
+                                    <span className={styles.labelStamp}>*</span>
+                                  )}
+                                </label>
+                                <div className={isEditing?styles.newFieldFormDiv:styles.fieldFormDiv}>
+                                  <input
+                                    name="timeLine"
+                                    type="numeric"
+                                    placeholder="Enter the expected delivery duration (in days)"
+                                    value={isEditing?(formik?.values.timeLine || ""):(`${formik?.values?.timeLine} Days`)}
+                                    onBlur={formik?.handleBlur}
+                                    onChange={formik?.handleChange}
+                                    disabled={!isEditing}
+                                    className={isEditing ? styles.fieldFormInput: styles.fieldFormNonEditInput}
+                                  />
+                                  {formik?.errors.timeLine &&
+                                    formik?.touched.timeLine && (
+                                      <div className={styles.fieldError}>
+                                        {formik?.errors.timeLine}
+                                      </div>
+                                  )}
                                 </div>
-                            )}
-                          </div>
-                        </div>
+                             </div>
+                          )
+                        }
+
+                        {/* product name para*/}
+                        {
+                          !isEditing && (
+                            <div className={styles.newFieldDiv}>
+                              <p className={styles.generalInfoLabel}>Submitted Product Name</p>
+                              <p className={styles.generalInfoValue}>{formik.values.productName}</p>
+                            </div>
+                          )
+                        }
+
+                        {/* submitted bid price para */}
+                        {
+                          !isEditing && (
+                            <div className={styles.newFieldDiv}>
+                              <p className={styles.generalInfoLabel}>Submitted Bid Price</p>
+                              <p className={styles.generalInfoValue}>{formik.values.amount}</p>
+                            </div>
+                          )
+                        }
+
+
                       </div>
                       
                       <div className={isEditing?styles.newFieldForm : styles.fieldForm}>
-                        <div className={ isEditing?styles.fieldDiv2:styles.editingFalseFieldDiv2}>
+                        {/* submitted bid timeline para */}
+                        {
+                          !isEditing && (
+                            <div className={styles.newFieldDiv}>
+                              <p className={styles.generalInfoLabel}>Submitted Bid Timeline</p>
+                              <p className={styles.generalInfoValue}>{formik.values.timeLine}</p>
+                            </div>
+                          )
+                        }
+                        {/* submitted tnc */}
+                        {
+                          !isEditing && (
+                            <div className={styles.newFieldDiv}>
+                              <p className={styles.generalInfoLabel}>Submitted Terms And Condition</p>
+                              <p className={styles.generalInfoValue}>
+                                {formik.values.tnc.length > 100 ? (
+                                  <div>
+                                    <span>{formik.values.tnc.substring(0,150) + "..."}</span>
+                                    <span id="tnc-tooltip" className={styles.viewmoreSpan}>view more</span>
+                                    <Tooltip
+                                    content={formik?.values?.tnc}
+                                    anchorId="tnc-tooltip"
+                                    place="bottom-start"
+                                    delayHide={500}
+                                    className={styles.toolTip}
+                                    />
+                                  </div>
+                                ) : (
+                                  formik.values.tnc
+                                )}
+                              </p>
+
+                            </div>
+                          )
+                        }
+                        {
+                          isEditing && (
+                          <div className={ isEditing?styles.fieldDiv2:styles.editingFalseFieldDiv2}>
                           <label className={styles.fieldFormLabel}>
                             {getFieldLabel("Terms And Condition")}
                             {isEditing && (
@@ -375,7 +576,7 @@ const BidDetails = () => {
                           </label>
                          {
                           isEditing? (
-                             <textarea
+                            <textarea
                             className={styles.formInput}
                             rows={5}
                             name={`tnc`}
@@ -397,6 +598,8 @@ const BidDetails = () => {
                             </span>
                           )}
                         </div>
+                          )
+                        }
                       </div>
 
                       {isEditing && (
